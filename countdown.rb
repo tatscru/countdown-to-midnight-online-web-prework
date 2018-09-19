4b50af8459f5b5720da9c7e4fb13c57553d19a69
tatiana.mojica <tatiana.mojica@bustle.com>
2018-09-19 00:05:55 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
-end
+def countdown(number)
+  while countdown = 10 
+  puts "#{number} SECOND (S)!"
+  countdown -= 1 
+  end
+  puts "HAPPY NEW YEAR!"
+end 
+
+def countdown_with_sleep(number)
+  while 
+  puts 
+end 
+end 
